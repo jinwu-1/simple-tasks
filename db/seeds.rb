953a090ffef0d26e082e064953ddf6a50a3dd951
jinwu-1 <jinn.wuu@gmail.com>
2021-03-24 18:02:14 -0400

    'seed'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.new(first_name: "Abe", last_name: "Banner", password: "asdf", email: "a.banner@asdf.com")
+User.new(first_name: "Isaac", last_name: "Fran", password: "asdf", email: "i.fran@asdf.com")
